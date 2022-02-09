4af8e6cb4b24874263c17e1a990a28b3763a4816
Jjones32 <98495284+Jjones32@users.noreply.github.com>
2022-02-09 00:20:55 -0500

    firt push all the way through

---
+++ DiceRollerLab/DiceRollerLab/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DiceRollerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Welcome to the Grand Circus Casino!");
+                Console.WriteLine("How many sides would you like each die to have?");
+                int sides = Convert.ToInt32(Console.ReadLine());
+
+                roll(sides);
+
+                Console.WriteLine("Would you like to restart? (y/n)");
+                var userAnswr = Console.ReadLine().ToLower();
+                if (userAnswr == "n")
+                {
+                    break;
+                }
+            }
+            while (true); 
+
+            
+
+               
+
+             
+        }
+        public static void roll(int sides)
+        {
+            Random random = new Random();
+            int dice_1 = random.Next(1, sides);
+            int dice_2 = random.Next(1, sides);
+            if (sides == 6)
+            {
+                SixSideDie(dice_1, dice_2);
+            }
+            Console.WriteLine("dice 1 = " + dice_1 + " dice 2 = " + dice_2 + Environment.NewLine);
+        }
+        public static void SixSideDie(int dieOne, int dieTwo)
+        {
+            int sum = dieOne + dieTwo;
+            if (dieOne == 1 && dieTwo == 1)
+            {
+                Console.WriteLine("You rolled SNake Eyes!");
+            }
+            if (dieOne == 1 && dieTwo == 2 || dieOne == 2 && dieTwo == 1)
+            {
+                Console.WriteLine("You have rolled an Ace Deuce!");
+            }
+            if (dieOne == 6 && dieTwo == 6)
+            {
+                Console.WriteLine("You rolled Box Cars!");
+            }
+            if (sum == 7 || sum == 11)
+            {
+                Console.WriteLine("You WIN!");
+            }
+            if (sum == 2 || sum == 3 || sum == 12)
+            {
+                Console.WriteLine("You have rolled Craps!");
+            }
+            
+
+            
+        }
+    }
+}
---
+++ NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NumberAnalyzerLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var y = true;
+            
+            
+
+
+                Console.WriteLine("Hello, Please enter your name");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Hello, " + name + " please enter a number between 1 and 100");
+                Console.WriteLine(" ");
+                string userInput = Console.ReadLine();
+                Console.WriteLine(" ");
+                int numInput = int.Parse(userInput);
+                var answr = numInput % 2;
+                    Console.WriteLine("Would you like to run the analyzer again? (y/n)");  
+
+
+
+                if (answr == 0)
+                {
+                    Console.WriteLine("Number is Even,");
+                }
+                if (answr != 0)
+                {
+                    Console.WriteLine("Number is Odd");
+                }
+                if (numInput >= 2 && numInput <= 24)
+                {
+                    Console.WriteLine("Your number " + numInput + " is even and less than 25!");
+                }
+                if (numInput >= 26 && numInput <= 60)
+                {
+                    Console.WriteLine("Your number " + numInput + " is even and between 26 and 60!");
+                }
+                if (numInput >= 60)
+                {
+                    Console.WriteLine("Your number " + numInput + " is even greater than 60!");
+                }
+              
+                    if (numInput <= 60 && numInput >= 1)
+                    {
+                        Console.WriteLine("Your number " + name + numInput + " is odd and less than 60!");
+                    }
+                    if (numInput >= 60)
+                    {
+                        Console.WriteLine("Your number " + numInput + " is odd and greater than 60!");
+
+                    }
+
+
+        }
+    }
+}
