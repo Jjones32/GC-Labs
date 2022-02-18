5480e35a29c2bb3cce02bfc9d60bd050b6930414
Jjones32 <98495284+Jjones32@users.noreply.github.com>
2022-02-17 22:12:42 -0500

    database 2 attempt 2

---
+++ StudentDatabase2/StudentDatabaseLab/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, for this database each student correlates to a number. Please choose a number 1-3 to view that students data.");
+var index = Convert.ToInt32(Console.ReadLine())-1;
+
+
+//List<string> name = new List<string>();
+string[] nameArray = new string[4] { "Jerry", "Beth", "Rick", "Morty" };
+
+Console.WriteLine(nameArray[index]);
+
+Console.WriteLine("You chose " + index + 1 + " which is " + nameArray[index] +
+    " What would you like to know? Enter 'hometown' or 'favorite food'.");
+
+//foreach()
+//{
+
+//}
+
+
+
+
+
+
+
+//List<string> hometown = new List<string>();
+string[] hometownArray = new string[4] { "Detroit", "Columbus", "Springfield", "Pittsburgh" };
+
+//List<string> favoriteFood = new List<string>();
+
